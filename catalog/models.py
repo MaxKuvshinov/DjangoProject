5a4586d4549125abcb7c527e9d10165b2808a783
Max Kuvshinov <maksim.kuvshinov94@gmail.com>
2024-10-03 01:58:47 +0300

    final version

--- catalog/models.py
+++ catalog/models.py
@@ -18,7 +18,9 @@ class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name="Название продукта")
     description = models.TextField(verbose_name="Описание продукта", blank=True, null=True)
     image = models.ImageField(upload_to="catalog/image", blank=True, null=True, verbose_name="Изображение")
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name="Категория", related_name="products")
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Категория", related_name="products"
+    )
     price = models.FloatField(verbose_name="Цена", help_text="Введите цену")
     created_at = models.DateField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateField(auto_now=True, verbose_name="Дата последнего изменения")
--- config/settings.py
+++ config/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
@@ -127,6 +128,10 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = (BASE_DIR / "static",)
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
