8f601fbb15e52a4e6413f513a6af83eb96dc5d9d
Max Kuvshinov <maksim.kuvshinov94@gmail.com>
2024-11-12 14:01:15 +0300

    configured caching for the page that displays information about one product in the file 'urls.py'

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,3 +1,5 @@
+from django.views.decorators.cache import cache_page
+
 from catalog.apps import CatalogConfig
 from catalog.views import (
     CatalogContactsView,
@@ -14,7 +16,7 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path("home/", CatalogHomeView.as_view(), name="home"),
     path("contacts/", CatalogContactsView.as_view(), name="contacts"),
-    path("product_detail/<int:pk>/", CatalogDetailView.as_view(), name="product_detail"),
+    path("product_detail/<int:pk>/", cache_page(60)(CatalogDetailView.as_view()), name="product_detail"),
     path("product_create/", ProductCreateView.as_view(), name="product_create"),
     path("product_update/update/<int:pk>/", ProductUpdateView.as_view(), name="product_update"),
     path("product_delete/delete/<int:pk>/", ProductDeleteView.as_view(), name="product_delete"),
